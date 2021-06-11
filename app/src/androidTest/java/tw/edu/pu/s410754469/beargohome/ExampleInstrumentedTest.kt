7d72c0fd82d6cc2a9a0155fbddba2531a7fd7b6a
kellt <s1075446@gm.pu.edu.tw>
2021-06-11 18:06:33 +0800

    0611

---
+++ app/src/androidTest/java/tw/edu/pu/s410754469/beargohome/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package tw.edu.pu.s410754469.beargohome
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("tw.edu.pu.s410754469.beargohome", appContext.packageName)
+    }
+}
