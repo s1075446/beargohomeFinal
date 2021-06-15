75821e3fb49a6c40e43f64050184dc0fce287963
kellt <s1075446@gm.pu.edu.tw>
2021-06-15 22:01:25 +0800

    0615

---
+++ app/src/main/java/tw/edu/pu/s410754469/beargohome/MainActivity62.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.s410754469.beargohome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity62 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main62)
+    }
+}
